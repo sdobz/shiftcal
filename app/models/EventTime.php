4dbe6d13cbdfda9305b635cb1403bdb92a4d7d96
Josh Hetrick <jhetrick@crowdcompass.com>
2018-03-23 20:05:54 -0700

    hacky way for indicating featured events

--- app/models/EventTime.php
+++ app/models/EventTime.php
@@ -80,14 +80,55 @@ class EventTime extends fActiveRecord {
         $caldaily_id = $this->getPkid();
         return "$base/event-" . $caldaily_id;
     }
-    
+
+    private function getFeatured() {
+        $id = $this->getId();
+        $year = $this->getEventdate()->format('Y');
+
+        if ($year == null) {
+            return null;
+        }
+
+        // Ideally this would be stored in the db.
+        // But AFAIK these featured events were just hand-marked on the classic cal.
+        // So here's a hard-coded list of featured events pulled from that cal.
+        $featured = [
+            // I don't see any featured events for 2009 or earlier
+            '2010' =>
+                [ 1603, 1604, 1321, 1545, 1574 ], // BonB x2, WNBR, Sunday Parkways, MCBF
+            '2011' =>
+                [ 2142, 2109, 2110, 2191 ], // Kickoff, WNBR, MCBF, SP
+            '2012' =>
+                [ 2774, 3145, 2787, 2788, 2789, 2807, 2776 ], // Kickoff ride, Kickoff party, BonB x4
+            '2013' =>
+                [ 3664, 3807, 3840, 3465, 3700, 3596 ], // // Kickoff ride, Kickoff party, WNBR event at PAM, WNBR, SP, MCBF
+            '2014' =>
+                [ 4294, 4477, 4478, 4479, 4480, 4129, 4150 ], // Kickoff, BonB x4, WNBR, MCBF
+            '2015' =>
+                [ 4544, 4982, 4983, 4984, 4552, 5098, 4769 ], // Kickoff, BonB x3, MCBF, MCBF to WNBR, WNBR
+            '2016' =>
+                [ 5467, 5469, 5463, 5464 ], // Kickoff, MMR, WNBR, SP
+            '2017' =>
+                [ 6216, 6233 ], // Kickoff, SP
+            '2018' =>
+                [ 6798, 6804, 6799 ], // Kickoff, WNBR, SP
+        ];
+
+        if ( in_array($id, $featured[$year]) ) {
+            return true;
+        } else {
+            return null;
+        }
+    }
+
     public function toEventSummaryArray() {
-	$eventArray = $this->getEvent()->toArray();
-	$eventArray['date'] = $this->getFormattedDate();
-	$eventArray['caldaily_id'] = $this->getPkid();
-	$eventArray['shareable'] = $this->getShareable();
-	$eventArray['cancelled'] = $this->getEventstatus() == 'C';
-	$eventArray['newsflash'] = $this->getNewsflash();
+        $eventArray = $this->getEvent()->toArray();
+        $eventArray['date'] = $this->getFormattedDate();
+        $eventArray['caldaily_id'] = $this->getPkid();
+        $eventArray['shareable'] = $this->getShareable();
+        $eventArray['cancelled'] = $this->getEventstatus() == 'C';
+        $eventArray['newsflash'] = $this->getNewsflash();
+        $eventArray['featured'] = $this->getFeatured();
 	return $eventArray;
     }
 }
